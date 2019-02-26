696dc49df53732c496dc03f90d46603b853d0e5e
X <z.xie4@umiami.edu>
2019-02-26 16:45:17 -0600

    change bedtools path

--- Canonical/grab_sequence.sh
+++ Canonical/grab_sequence.sh
@@ -8,6 +8,6 @@ CLl=$(($CLr+1))
 
 cat $splice_table | awk -v CLl=$CLl -v CLr=$CLr '{print $3"\t"($5-CLl)"\t"($6+CLr)}' > temp.bed
 
-bedtools getfasta -bed temp.bed -fi $ref_genome -fo $sequence -tab
+./bedtools getfasta -bed temp.bed -fi $ref_genome -fo $sequence -tab
 
 rm temp.bed
