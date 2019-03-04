306c38d4d6eed8792581688cffa1f81e91a99d82
ZiqianXie <z.xie4@umiami.edu>
2019-03-04 14:54:27 -0600

    integrate ensemble training in train_model

--- Canonical/train_model.py
+++ Canonical/train_model.py
@@ -51,12 +51,8 @@ CL = 2 * np.sum(AR*(W-1))
 assert CL <= CL_max and CL == int(sys.argv[1])
 print ("\033[1mContext nucleotides: %d\033[0m" % (CL))
 print ("\033[1mSequence length (output): %d\033[0m" % (SL))
-callback = Callback(Chromosome())
+callback = Callback()
 
-model = SpliceAI(L, W, AR)
-model.summary()
-model_m = model
-model_m.compile(loss=categorical_crossentropy_2d, optimizer='adam')
 
 ###############################################################################
 # Training and validation
@@ -67,121 +63,126 @@ except OSError:
     h5f = h5py.File(data_dir + 'train_all_1.h5', 'r')
 
 num_idx = len(h5f.keys())//2
+h5f.close()
 idx_all = np.random.permutation(num_idx)
 idx_train = idx_all[:int(0.9*num_idx)]
 idx_valid = idx_all[int(0.9*num_idx):]
 
-EPOCH_NUM = 10*len(idx_train)
+EPOCH_NUM = 10
 
 start_time = time.time()
 
-
-for epoch_num in range(EPOCH_NUM):
-    if epoch_num > 0 and epoch_num % len(idx_train) == 0:
-        callback()
-
-    idx = np.random.choice(idx_train)
-
-    X = h5f['X' + str(idx)][:]
-    Y = h5f['Y' + str(idx)][:]
-
-    Xc, Yc = clip_datapoints(X, Y, CL, N_GPUS) 
-    model_m.fit(Xc, Yc, batch_size=BATCH_SIZE, verbose=0)
-
-
-    if (epoch_num+1) % len(idx_train) == 0:
-        # Printing metrics (see utils.py for details)
-
+for model_idx in range(1, 6):
+    model_m = SpliceAI(L, W, AR)
+    model_m.compile(loss=categorical_crossentropy_2d, optimizer='adam')
+    for epoch_num in range(EPOCH_NUM):
+        try:
+            h5f = h5py.File(data_dir + 'train_all_0.h5', 'r')
+        except OSError:
+            h5f = h5py.File(data_dir + 'train_all_1.h5', 'r')
+        if epoch_num > 0:
+            callback()
+        for _ in range(len(idx_train)):
+            idx = np.random.choice(idx_train)
+            X = h5f['X' + str(idx)][:]
+            Y = h5f['Y' + str(idx)][:]
+        
+            Xc, Yc = clip_datapoints(X, Y, CL, N_GPUS) 
+            model_m.fit(Xc, Yc, batch_size=BATCH_SIZE, verbose=0)
+    
+    
+        
+            # Printing metrics (see utils.py for details)
+    
         print ("--------------------------------------------------------------")
         print ("\n\033[1mValidation set metrics:\033[0m")
-
+    
         Y_true_1 = [[] for t in range(1)]
         Y_true_2 = [[] for t in range(1)]
         Y_pred_1 = [[] for t in range(1)]
         Y_pred_2 = [[] for t in range(1)]
-
+    
         for idx in idx_valid:
-
+    
             X = h5f['X' + str(idx)][:]
             Y = h5f['Y' + str(idx)][:]
-
+    
             Xc, Yc = clip_datapoints(X, Y, CL, N_GPUS)
             Yp = model_m.predict(Xc, batch_size=BATCH_SIZE)
-
+    
             if not isinstance(Yp, list):
                 Yp = [Yp]
-
+    
             for t in range(1):
-
+    
                 is_expr = (Yc[t].sum(axis=(1,2)) >= 1)
-
+    
                 Y_true_1[t].extend(Yc[t][is_expr, :, 1].flatten())
                 Y_true_2[t].extend(Yc[t][is_expr, :, 2].flatten())
                 Y_pred_1[t].extend(Yp[t][is_expr, :, 1].flatten())
                 Y_pred_2[t].extend(Yp[t][is_expr, :, 2].flatten())
-
+    
         print ("\n\033[1mAcceptor:\033[0m")
         for t in range(1):
             print_topl_statistics(np.asarray(Y_true_1[t]),
                                   np.asarray(Y_pred_1[t]))
-
+    
         print ("\n\033[1mDonor:\033[0m")
         for t in range(1):
             print_topl_statistics(np.asarray(Y_true_2[t]),
                                   np.asarray(Y_pred_2[t]))
-
+    
         print ("\n\033[1mTraining set metrics:\033[0m")
-
+    
         Y_true_1 = [[] for t in range(1)]
         Y_true_2 = [[] for t in range(1)]
         Y_pred_1 = [[] for t in range(1)]
         Y_pred_2 = [[] for t in range(1)]
-
+    
         for idx in idx_train[:len(idx_valid)]:
-
+    
             X = h5f['X' + str(idx)][:]
             Y = h5f['Y' + str(idx)][:]
-
+    
             Xc, Yc = clip_datapoints(X, Y, CL, N_GPUS)
             Yp = model_m.predict(Xc, batch_size=BATCH_SIZE)
-
+    
             if not isinstance(Yp, list):
                 Yp = [Yp]
-
+    
             for t in range(1):
-
+    
                 is_expr = (Yc[t].sum(axis=(1,2)) >= 1)
-
+    
                 Y_true_1[t].extend(Yc[t][is_expr, :, 1].flatten())
                 Y_true_2[t].extend(Yc[t][is_expr, :, 2].flatten())
                 Y_pred_1[t].extend(Yp[t][is_expr, :, 1].flatten())
                 Y_pred_2[t].extend(Yp[t][is_expr, :, 2].flatten())
-
+    
         print ("\n\033[1mAcceptor:\033[0m")
         for t in range(1):
             print_topl_statistics(np.asarray(Y_true_1[t]),
                                   np.asarray(Y_pred_1[t]))
-
+    
         print ("\n\033[1mDonor:\033[0m")
         for t in range(1):
             print_topl_statistics(np.asarray(Y_true_2[t]),
                                   np.asarray(Y_pred_2[t]))
-
+    
         print ("Learning rate: %.5f" % (kb.get_value(model_m.optimizer.lr)))
         print ("--- %s seconds ---" % (time.time() - start_time))
         start_time = time.time()
-
+    
         print ("--------------------------------------------------------------")
-
+    
         model.save('./Models/SpliceAI' + sys.argv[1]
-                   + '_c' + sys.argv[2] + '.h5')
-
+                   + '_c{}'.format(model_idx) + '.h5')
+    
         if (epoch_num+1) >= 6*len(idx_train):
             kb.set_value(model_m.optimizer.lr,
                          0.5*kb.get_value(model_m.optimizer.lr))
-            # Learning rate decay
-
-h5f.close()
+                # Learning rate decay
+        h5f.close()
         
 ###############################################################################
 
